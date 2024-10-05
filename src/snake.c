09a627652c271eeb20b056e3f06e8d0ff0df31a7
Кристина <kristina@Air-Kristina.IGD_Rostelecom>
2024-10-06 05:36:47 +0600

    первый коммит

---
+++ src/snake.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <unistd.h>
+
+#include "snake.h"
+
+int main(){
+    
+}
+
+int input(){
+    
+}
+
+int init_matrix(){
+    
+}
+
+int render(){
+    
+}
+
+
+
+
+
